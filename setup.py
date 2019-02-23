bcce5e07e6fb5c12d008120f000452c64d80de8f
Scott Baker <smbaker@gmail.com>
2019-02-22 21:06:47 -0800

    pypi gets mad at me if I try to upload a package called Prometheus. go figure.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ plugin_package = "octoprint_prometheus"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Prometheus"
+plugin_name = "octoprint-prometheus"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0.0"
